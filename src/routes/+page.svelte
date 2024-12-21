b1cb0c1e21317e8e41fd43e765d1b06416c7a74d
RTMAV <rtmav.codes@gmail.com>
2024-12-21 03:22:24 -0800

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -182,25 +182,19 @@
                     <form target="_blank" action="https://formsubmit.co/rotem.avisar@gmail.com" method="POST" class="w-full">
                         <div class="form-group">
                             <div class="form-row">
-                                <label for="email" class="block text-sm font-bold">
-                                    Your email
-                                </label>
+                                <label for="email" class="block text-sm font-bold">Your email</label>
                                 <input type="email" name="email" class="form-control shadow-sm bg-gray-50 border border-gray-300 
                                 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block 
                                 w-full p-2.5" placeholder="name@email.com" required>
                             </div>
                             <div class="form-row">
-                                <label for="subject" class="block text-sm font-medium pt-2">
-                                    Subject
-                                </label>
+                                <label for="subject" class="block text-sm font-medium pt-2">Subject</label>
                                 <input type="text" name="subject" class="form-control block p-3 w-full text-sm text-gray-900 
                                 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 
                                 focus:border-primary-500" placeholder="Let us know how we can help you" required>
                             </div>
                             <div class="sm:col-span-2 form-row">
-                                <label for="message" class="block text-sm font-medium pt-2">
-                                    Your message
-                                </label>
+                                <label for="message" class="block text-sm font-medium pt-2">Your message</label>
                                 <textarea name="message" rows="6" class="form-control block p-2.5 w-full text-sm text-gray-900
                                  bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 
                                  focus:border-primary-500" placeholder="Leave a comment...">
