50fbf92b73c88f0658a3e803d6041cf122f0b550
rtmav <rtmav.codes@gmail.com>
2026-04-12 10:03:47 -0400

    chore: remove .vercel/output to fix Vercel deploy 500 error

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.NHUbYKv5.js","_app/immutable/chunks/BQRd7S7z.js","_app/immutable/chunks/C8iQ9IWt.js","_app/immutable/chunks/DVQ6k0tf.js","_app/immutable/chunks/CDXu8Erm.js","_app/immutable/chunks/CHhwE8-t.js","_app/immutable/chunks/BHsBF2M1.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/6.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 6;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/walletguard/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/6.CfEM_pBp.js","_app/immutable/chunks/BQRd7S7z.js","_app/immutable/chunks/C8iQ9IWt.js","_app/immutable/chunks/DVQ6k0tf.js","_app/immutable/chunks/CDXu8Erm.js","_app/immutable/chunks/CHhwE8-t.js","_app/immutable/chunks/29JTwZ4f.js","_app/immutable/chunks/CWlsImmY.js","_app/immutable/chunks/BQwqyRFK.js"];
-export const stylesheets = ["_app/immutable/assets/GalleryThumbnail.BIYRPbym.css","_app/immutable/assets/6.D_ebwUb-.css"];
-export const fonts = [];
