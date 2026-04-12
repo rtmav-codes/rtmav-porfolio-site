a3f5e67e41940d01c707ceb310fc84abeb3e01da
rtmav <rtmav.codes@gmail.com>
2026-04-12 09:51:42 -0400

    add evoq ai to featured projects

--- src/lib/data/featured-projects.js
+++ src/lib/data/featured-projects.js
@@ -1,5 +1,16 @@
 export default [
     {
+        "title": "Evoq AI",
+        "description": "Solo Developed a AI consulation platform for client looking to color their hair. The platform allows users to upload a photo of themselves and get an AI generated before and after photo of themselves based on survey results or an uploaded photo.",
+        "demoUrl": "https://ai.evoqsalon.com/",
+        "video": {
+            "url": null
+        },
+        "image": {
+            "url": "/evoq-ai-featured.png"
+        }
+    },
+    {
         "title": "Surgical Theater",
         "description": "Worked as part of a team to design and build med tech dashboards AR/VR surgery simulators according to specifications from neurosurgeons at HOAG hospital in Newport Beach, CA.",
         "demoUrl": "surgicaltheater",
@@ -15,12 +26,12 @@ export default [
         "description": "Created a landing page in Webflow for a Web3 company that aims to make the cyprto space safer by preventing common scams such as wallet drainers and honey pots.",
         "demoUrl": "walletguard",
         "video": {
-            "url": null 
+            "url": null
         },
         "image": {
             "url": "/wallet-guard-after/wallet-guard-browser-extension-cover.png"
         }
-    },{
+    }, {
         "title": "Speak2Me",
         "description": "Led a team that developed an iOS app using SvelteNative and NativeScript that utilizes ARKit to track facial movements of patients and created exercises used for speech therapy. The goal of this app is to be used by therapists to reduce certain pain points such as paper curriculums, subjective tracking of facial asymetry for stroke patients and billing.",
         "demoUrl": "speak2me",
@@ -28,7 +39,7 @@ export default [
         "image": {
             "url": "/speak2me-featured.png"
         }
-    },{
+    }, {
         "title": "Trident Copy Trader",
         "description": "Created a Fimga UX/UI Design for an administrative dashboard as part of a team working on a copy trading platform. The platform will allow Trident Trading to copy their algorithmic trading accounts to client exchange accounts.",
         "demoUrl": null,
@@ -36,7 +47,7 @@ export default [
         "image": {
             "url": "/trident-copy.png"
         }
-    },{
+    }, {
         "title": "Symphony Robotics System UX/UI Design",
         "description": "Created a Fimga UX/UI Design for a surgical laser company. The platform will allow surgeons to perform planning for upcoming surgeries with a patient management system and a UI to create surgical paths using different types of medical scans and images.",
         "demoUrl": null,
